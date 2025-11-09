81c4195d216b1fdca75dd6be6c3e95b6062ff5d5
uevgh <uveghobamien@gmail.com>
2025-11-09 04:28:05 +0000

    Add EF Core and PostgreSQL support to Catalog.Infrastructure
    Updated Catalog.Infrastructure to target .NET 8.0 and added
    Entity Framework Core with PostgreSQL integration. Introduced
    `CatalogDbContext` and `ProductConfiguration` for managing
    and configuring the `Product` entity. Seeded initial data for
    the `Products` table. Added necessary NuGet packages and
    project references. Created a placeholder `Class1` file.

---
+++ Catalog.Infrastructure/Configurations/ProductConfiguration.cs
@@ -0,0 +1,28 @@
+﻿
+
+using Catalog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Catalog.Infrastructure.Configurations;
+
+public class ProductConfiguration:IEntityTypeConfiguration<Product>
+{
+   
+
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Products");
+        builder.Property(p => p.Name).IsRequired().HasMaxLength(200);
+        builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
+
+        builder.HasData(
+            new Product("Lenovo", 999.99m, 10)
+            {
+                Id = Guid.NewGuid()
+            },
+
+            new Product("Iphone 15", 800.77m, 10) { Id = Guid.NewGuid() }
+            );
+    }
+}
---
+++ Catalog.Infrastructure/Data/CatalogDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Catalog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Infrastructure.Data;
+
+public class CatalogDbContext:DbContext
+{
+
+    public CatalogDbContext( DbContextOptions<CatalogDbContext>options) : base(options) { }
+
+
+    public DbSet<Product> Products { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        //get enetity product builder on modelcreating
+        modelBuilder.Entity<Product>((entity) =>
+        {
+            //name of table
+            entity.ToTable("Products");
+            entity.Property(p => p.Name).IsRequired().HasMaxLength(200);
+            entity.Property(p => p.Price).HasColumnType("decimal(18,2)");
+
+            entity.HasData(
+                new Product("Lenovo", 999.99m, 10)
+                {
+                    Id = Guid.NewGuid()
+                },
+
+                new Product("Iphone 15", 800.77m, 10) { Id = Guid.NewGuid() }
+                );
+        }); 
+
+    }
+    
+}
