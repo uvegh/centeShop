4fdb22b088ad3ac4c5ab896dfa4f01bbf9fc6bf3
uevgh <uveghobamien@gmail.com>
2025-11-12 17:10:53 +0000

    Refactor solution and implement event-driven architecture
    - Updated `centeshop-micro.sln` to reflect new project references.
    - Removed `Class1` from `Ordering.Application`.
    - Added `OrderCreatedDomainEventHandler` with `MassTransit` and `MediatR` for event handling and publishing.
    - Upgraded `Ordering.Application` to .NET 8.0 and added necessary dependencies.
    - Updated `OrderingDbContext` to ignore `IDomainEvent` during model building.
    - Refactored `Catalog.API` to reference `Catalog.Infrastructure`.
    - Configured RabbitMQ in `Ordering.API` for `MassTransit`.
    - Removed `LoadTests` project and associated files.
    - Added `PaymentCreatedIntegrationEvent` and `CreateOrder` class.
    - Minor refactoring and formatting changes across the solution.

---
+++ src/Ordering/Ordering.Infrastructure/Common/DomainEventAdapter.cs
@@ -0,0 +1,12 @@
+﻿
+
+using MediatR;
+using Shared.Library.Events;
+
+namespace Ordering.Infrastructure.Common;
+
+// This wraps any domain event into a MediatR-compatible type
+public record DomainEventNotification<TDomainEvent>(TDomainEvent DomainEvent):INotification
+    where TDomainEvent: BaseDomainEvent;
+
+
--- src/Ordering/Ordering.Infrastructure/Common/DomainEventDispatcher.cs
+++ src/Ordering/Ordering.Infrastructure/Common/DomainEventDispatcher.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Ordering.Domain.Common;
+using Shared.Library.Events;
 
 namespace Ordering.Infrastructure.Common;
 
@@ -20,7 +21,12 @@ public class DomainEventDispatcher
         //publish each event through mediatr
         foreach( var domainEvent in domainEvents)
         {
-            await _mediator.Publish(domainEvent);
+            //•	uses Activator.CreateInstance to build a DomainEventNotification<T> wrapper around each domain event so it can be published through MediatR.
+//	Reason: domain events implements  IDomainEvent interface, but MediatR //expects an INotification(or a generic DomainEventNotification<T> type) so handlers can subscribe to DomainEventNotification<TEvent>.
+
+            var notification = Activator.CreateInstance(typeof(DomainEventNotification<>).MakeGenericType(domainEvent.GetType()),domainEvent);
+
+            await _mediator.Publish(((INotification)notification!));
         }
 
         foreach(var  aggregate in aggregrates)
