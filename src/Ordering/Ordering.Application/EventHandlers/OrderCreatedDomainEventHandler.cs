83304bc72e1357628d6a05618a83eb205d77df6c
uevgh <uveghobamien@gmail.com>
2025-11-12 17:13:17 +0000

    Refactor event handling and integrate MassTransit
    Enhanced `OrderCreatedDomainEventHandler` to use MediatR and MassTransit for event-driven architecture. Added logging with `ILogger` and implemented event publishing via RabbitMQ. Removed unused `Class1` and introduced a placeholder `CreateOrder` class. Updated project dependencies to include `MassTransit`, `MediatR`, and logging libraries. Added a project reference to `Ordering.Infrastructure`.

--- src/Ordering/Ordering.Application/EventHandlers/OrderCreatedDomainEventHandler.cs
+++ src/Ordering/Ordering.Application/EventHandlers/OrderCreatedDomainEventHandler.cs
@@ -1,10 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.Extensions.Logging;
+using MassTransit;
 
+using MediatR;
+using Ordering.Infrastructure.Common;
+using Ordering.Domain.Events;
+using Shared.Library.Events;
 
 namespace Ordering.Application.EventHandlers;
-
-public class OrderCreatedDomainEventHandler
+//mediatr automatically detects this after attaching the notificationhandler and with reference to the domaainevenr
+public class OrderCreatedDomainEventHandler:INotificationHandler<DomainEventNotification<OrderCreatedDomainEvent>>
 {
+    private readonly ILogger<OrderCreatedDomainEventHandler> _logger;
+    private readonly IPublishEndpoint _publishEndpoint;
+    public OrderCreatedDomainEventHandler(ILogger<OrderCreatedDomainEventHandler> logger, IPublishEndpoint publishEndpoint )
+    {
+        _logger = logger;
+        _publishEndpoint = publishEndpoint;
+
+
+
+    }
+
+    //event handler
+    public async Task Handle(DomainEventNotification<OrderCreatedDomainEvent> notification, CancellationToken ct)
+    {
+        var @event = notification.DomainEvent;
+        _logger.LogInformation("order created domain event notification, orderId={OrderId} and occured at {OccuredAt}", @event.OrderId, @event.OccuredAt);
+
+        var integrationEvent = new PaymentCreatedIntegrationEvent(@event.OrderId, @event.UserId, @event.TotalAmount);
+        _logger.LogInformation("Publish payment creation event order {OrderId} at {OccurredAt}", @event.OrderId, @event.OccuredAt);
+        await _publishEndpoint.Publish(integrationEvent, ct);
+        //set task to completed after logging
+        await Task.CompletedTask;
+    }
+ 
+    
 }
