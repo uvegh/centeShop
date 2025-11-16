92cfd5e1deda5ee974d8d4444e2aa50795d71ecf
uevgh <uveghobamien@gmail.com>
2025-11-16 22:43:26 +0000

    Update CreateProductCommand to return ProductDto
    Updated `using` directives to include `Catalog.API.DTOs` for DTO references. Modified `CreateProductCommand` to implement `IRequest<ProductDto>` instead of `IRequest`, enabling the command to return a `ProductDto` object.

--- src/Services/Catalog/Catalog.Application/Features/Command/CreateProductCommand.cs
+++ src/Services/Catalog/Catalog.Application/Features/Command/CreateProductCommand.cs
@@ -1,5 +1,6 @@
-﻿using MediatR;
+﻿using Catalog.API.DTOs;
+using MediatR;
 
 namespace Catalog.Application.Features.Command;
 
-public  record  CreateProductCommand( string Name, decimal Price, int StockQuantity) :IRequest;
+public  record  CreateProductCommand( string Name, decimal Price, int StockQuantity) :IRequest<ProductDto>;
