a2ae2d8a756ca6d97c7d207deb815e9d10863471
uevgh <uveghobamien@gmail.com>
2025-11-16 22:36:28 +0000

    Add ProductRepository implementation
    Implemented the `ProductRepository` class in the `Catalog.Infrastructure.Repositories` namespace, adhering to the `IProductRepository` interface.
    
    - Injected `CatalogDbContext` via constructor for database operations.
    - Added methods:
      - `AddAsync`: Adds a product and saves changes.
      - `ExistsAsync`: Checks if a product exists by ID.
      - `GetAllAsync`: Retrieves all products.
      - `GetByIdAsync`: Placeholder for fetching a product by ID.
      - `SaveChangesAsync`: Saves changes to the database.
    - Included necessary `using` directives for dependencies.

---
+++ Catalog.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,55 @@
+﻿using Catalog.Domain.Entities;
+using Catalog.Domain.Interfaces;
+using Catalog.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Shared.Library.Model.Pagination;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Catalog.Infrastructure.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly CatalogDbContext _dbContext;
+    
+    public ProductRepository(CatalogDbContext dbContext)
+    {
+        _dbContext = dbContext;
+
+
+    }
+
+   public  async Task AddAsync(Product product,CancellationToken ct)
+    {
+        
+        await _dbContext.Set<Product>().AddAsync(product);
+        await SaveChangesAsync(ct);
+    }
+
+    public async Task<bool> ExistsAsync(Guid id)
+    {
+        var product = await _dbContext.Set<Product
+            >().FirstOrDefaultAsync(x=>x.Id==id);
+      
+        return true && product != null;
+  
+    }
+
+    public async Task<List<Product>> GetAllAsync(CancellationToken ct)
+    {
+        var products = await _dbContext.Set<Product>().ToListAsync();
+        return products;
+    }
+
+    public async Task<Product?> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+  public async  Task SaveChangesAsync(CancellationToken ct)
+    {
+        
+        await _dbContext.SaveChangesAsync(ct);
+    }
+
+  
+}
