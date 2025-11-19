48c5f0cb7f29a54a16e2de77048646a19a765031
uevgh <uveghobamien@gmail.com>
2025-11-19 01:59:06 +0000

    Add RedisConnectionFactory for Redis database access
    Introduced a new `RedisConnectionFactory` class in the
    `Cart.Infrastructure.Redis` namespace to manage Redis
    connections using the `StackExchange.Redis` library.
    
    - Added a `Lazy<ConnectionMultiplexer>` field to handle
      connection initialization.
    - Implemented a constructor to accept a Redis connection
      string and initialize the connection.
    - Added a `GetDatabase` method to retrieve an `IDatabase`
      instance for Redis operations.

---
+++ Cart.Infrastructure/Redis/RedisConnectionFactory.cs
@@ -0,0 +1,15 @@
+﻿
+using StackExchange.Redis;
+namespace Cart.Infrastructure.Redis;
+
+public  class RedisConnectionFactory
+{
+    private readonly Lazy<ConnectionMultiplexer> _connection;
+
+    public RedisConnectionFactory( string connectionStr)
+    {
+        //
+        _connection = new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(connectionStr));
+    }
+    public IDatabase GetDatabase() => _connection.Value.GetDatabase();
+}
