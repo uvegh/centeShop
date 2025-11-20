a36495b812b278669142fe92775534420d39637b
uevgh <uveghobamien@gmail.com>
2025-11-20 11:32:59 +0000

    Add RemoveItemCommand and its handler for cart operations
    Introduced a `RemoveItemCommand` record to handle item removal
    from a user's cart. Added `RemoveItemCommandHandler` to process
    the command, leveraging `ICartRespository` for repository
    operations and `ILogger` for logging. The handler retrieves the
    cart, checks for the item's existence, and removes it if found,
    returning a boolean result.

---
+++ src/Services/cart/Cart.Applicaiton/Features/Command/Cart/RemoveItem/RemoveItemCommand.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace Cart.Application.Features.Command.Cart.RemoveItem
+{
+    public record  RemoveItemCommand(Guid UserId, Guid ProductId) :IRequest<bool>;
+    
+   
+}
---
+++ src/Services/cart/Cart.Applicaiton/Features/Command/Cart/RemoveItem/RemoveItemCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿
+
+using Cart.Application.Features.Command.Cart.RemoveItem;
+using Cart.Domain.IRepository;
+
+namespace Cart.Application.Features.Command.Cart.RemoveItem;
+
+public class RemoveItemCommandHandler:IRequestHandler<RemoveItemCommand,bool>
+{
+    private readonly ICartRespository _cartRepo;
+    private readonly ILogger _logger;
+
+    public RemoveItemCommandHandler(ILogger<RemoveItemCommandHandler> logger, ICartRespository cartRespository)
+    {
+        _cartRepo = cartRespository;
+        _logger = logger;
+    }
+    public async Task<bool> Handle(RemoveItemCommand req, CancellationToken ct)
+    {
+        var cart = await _cartRepo.GetByUserIdAsync(req.UserId);
+        if (cart != null)
+        {
+            var item = cart.Items.FirstOrDefault(x => x.ProductId == req.ProductId);
+            if (item != null)
+            {
+
+
+                await _cartRepo.RemoveCartItemAsync(req.UserId, req.ProductId);
+                return true;
+            }
+            return false;
+        }
+        
+        return false;
+
+
+        
+    }
+}
