034f357e71f39d22b0973a2782d495287798a648
uevgh <uveghobamien@gmail.com>
2025-11-22 16:27:06 +0000

    create ordering interface

---
+++ src/Services/Ordering/Ordering.Domain/Interface/IOrderingRepository.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Ordering.Domain.Entities;
+
+namespace Ordering.Domain.Interface;
+
+public  interface IOrderingRepository
+{
+
+    Task<Order?> GetOrderByIdAsync(Guid id);
+    Task <List<Order>> GetAllAsync  (CancellationToken ct);
+    Task AddAsync(Order order, CancellationToken ct);
+    Task SaveChangesAsync(CancellationToken ct);
+    Task<bool> Exists(Guid id, CancellationToken ct);
+
+
+}
