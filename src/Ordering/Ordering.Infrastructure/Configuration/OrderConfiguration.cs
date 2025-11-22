f87f4f3789e15cbb3c379d8e2fd40052f0500583
uevgh <uveghobamien@gmail.com>
2025-11-22 16:28:49 +0000

    order and order item entity configuration

---
+++ src/Ordering/Ordering.Infrastructure/Configuration/OrderConfiguration.cs
@@ -0,0 +1,23 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Configuration;
+
+
+public class OrderConfiguration:IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> modelBuilder) {
+        modelBuilder.ToTable("Order");
+        modelBuilder.Property(p => p.UserId).IsRequired();
+        modelBuilder.HasMany(p => p.Items).WithOne().HasForeignKey(o => o.OrderId).OnDelete(DeleteBehavior.Cascade);
+
+
+        
+        modelBuilder.Property(p => p.Status).IsRequired();
+        modelBuilder.Property(p => p.TotalAmount).HasColumnType("decimal").IsRequired();
+        modelBuilder.Property(p => p.Address).IsRequired();
+    }
+}
---
+++ src/Ordering/Ordering.Infrastructure/Configuration/OrderItemConfiguration.cs
@@ -0,0 +1,20 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Configuration;
+
+
+public class OrderItemConfiguration:IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> modelBuilder) {
+        modelBuilder.ToTable("OrderItem");
+        modelBuilder.HasKey(p => p.Id);
+        modelBuilder.Property(p => p.ProductName).IsRequired();
+        modelBuilder.Property(p => p.Quantity).IsRequired();
+        modelBuilder.Property(p => p.UnitPrice).IsRequired();
+        modelBuilder.Property(p => p.ProductId);
+    }
+}
